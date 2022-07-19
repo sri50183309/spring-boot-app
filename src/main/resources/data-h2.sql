e6e256fa7f8d6162ac2063c8f6e823f282bff5c9
Sriram <sriram.mahalingam@gmail.com>
2022-07-19 21:36:05 +0530

    added sql script to insert at the start up ddl , insert scripts

--- src/main/resources/data-h2.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO theatre (id, name) VALUES (1, 'PVR');
-INSERT INTO theatre (id, name) VALUES (2, 'INOX');
-INSERT INTO theatre (id, name) VALUES (3, 'SATYAM');
-INSERT INTO theatre (id, name) VALUES (4, 'DEVI');
-INSERT INTO theatre (id, name) VALUES (5, 'IMAX');
