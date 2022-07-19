9683b9f5e4b1b35cb04c3480349d2505aa419720
Sriram <sriram.mahalingam@gmail.com>
2022-07-19 16:08:59 +0530

    added scan base package

--- src/main/java/com/sapient/xyz/bookingapp/BookingappApplication.java
+++ src/main/java/com/sapient/xyz/bookingapp/BookingappApplication.java
@@ -3,7 +3,7 @@ package com.sapient.xyz.bookingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sapient.xyz.bookingapp")
 public class BookingappApplication {
 
 	public static void main(String[] args) {
