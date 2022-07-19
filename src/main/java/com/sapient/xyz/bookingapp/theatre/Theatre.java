032978f72faad5d870b90ddd89f9dd35de0c5911
Sriram <sriram.mahalingam@gmail.com>
2022-07-19 20:41:51 +0530

    enabled h2console endpoint, provided insert scripts for theatres

--- src/main/java/com/sapient/xyz/bookingapp/theatre/Theatre.java
+++ src/main/java/com/sapient/xyz/bookingapp/theatre/Theatre.java
@@ -1,4 +1,18 @@
 package com.sapient.xyz.bookingapp.theatre;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class Theatre {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
 }
