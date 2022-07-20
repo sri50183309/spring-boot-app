7b6ce879ae80e242648dc6d04e05db5f906aac22
Sriram <sriram.mahalingam@gmail.com>
2022-07-20 14:11:58 +0530

    domain object fr endpoint response

---
+++ src/main/java/com/sapient/xyz/bookingapp/domain/MoviesInTheatre.java
@@ -0,0 +1,14 @@
+package com.sapient.xyz.bookingapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MoviesInTheatre {
+    private List<Theatre> theatres;
+}
